945f69039677b4cd080917d6fff5857c813c6017
Wenjundai0909 <253565545@qq.com>
2020-10-26 00:49:20 +0800

    django_learning1

---
+++ blog_sys/comment/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    nickname = forms.CharField(
+        label='昵称',
+        max_length=50,
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control', 'style': "width: 60%;"}
+        )
+    )
+    email = forms.CharField(
+        label='邮箱',
+        max_length=50,
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control', 'style': "width: 60%;"}
+        )
+    )
+    website = forms.CharField(
+        label='网站',
+        max_length=100,
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control', 'style': "width: 60%;"}
+        )
+    )
+    content = forms.CharField(
+        label='内容',
+        max_length=500,
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control', 'style': "width: 60%;"}
+        )
+    )
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if len(content) < 1:
+            raise forms.ValidationError('不可发布空评论！')
+        return content
+
+    class Meta:
+        model = Comment
+        fields = ['nickname', 'email', 'website', 'content']
+
---
+++ blog_sys/typeidea/urls.py
@@ -0,0 +1,51 @@
+"""typeidea URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from blog.views import IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView
+from config.views import links
+from .custom_site import custom_site
+
+urlpatterns = [
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
+    url(r'^links/$', links, name='links'),
+    url(r'^super_admin/', admin.site.urls, name='super-admin'),
+    url(r'^admin/', custom_site.urls, name='admin'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
+    url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+]
+
+
+# from django.contrib import admin
+# from django.conf.urls import url
+#
+# from blog.views import post_list, post_detail
+# from config.views import links
+# from .custom_site import custom_site
+#
+# urlpatterns = [
+#     url(r'^$', post_list, name='index'),
+#     url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+#     url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+#     url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+#     url(r'^links/$', links, name='links'),
+#     url(r'^super_admin/', admin.site.urls, name='super-admin'),
+#     url(r'^admin/', custom_site.urls, name='admin'),
+# ]
